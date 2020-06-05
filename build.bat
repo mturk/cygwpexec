942a89dbebb1ce5dce1bf8646e631d8e71716278
Mladen Turk <mturk@apache.org>
2020-06-05 16:52:55 +0200

    Rename files

--- build.bat
+++ build.bat
@@ -26,11 +26,13 @@ if /i "%~1" == "/x86" (
     set "MACHINE=X64"
     set "CFLAGS=/DWIN64 %CFLAGS%"
 )
+
 rem
 rem Clean previous build
 for %%i in (exe obj res pdb) do del /F /Q  %SNAM%.%%i 2>NUL
+if /i "%~1" == "/clean" exit /B 0 
 rem
-cl /nologo /O2 /Ob2 /Zi /MD /W3 /DWIN32 %CFLAGS% /DUNICODE /D_UNICODE /DCONSOLE /c posix2winexec.c /Fd%SNAM% /Fo%SNAM%.obj
-rc /l 0x409 /n /d "NDEBUG" %RCOPTS% /d STR_VERSION=\"%SVER%\" /d STR_INTNAME=\"%SNAM%\" /d CSV_VERSION=%CVER% /fo %SNAM%.res posix2winexec.rc
+cl /nologo /O2 /Ob2 /Zi /MD /W3 /DWIN32 %CFLAGS% /DUNICODE /D_UNICODE /DCONSOLE /c main.c /Fd%SNAM% /Fo%SNAM%.obj
+rc /l 0x409 /n /d "NDEBUG" %RCOPTS% /d STR_VERSION=\"%SVER%\" /d STR_INTNAME=\"%SNAM%\" /d CSV_VERSION=%CVER% /fo %SNAM%.res util.rc
 link /NOLOGO /OPT:REF /INCREMENTAL:NO /SUBSYSTEM:CONSOLE /MACHINE:%MACHINE% %LDFLAGS% %SNAM%.obj %SNAM%.res kernel32.lib %EXTRA_LIBS% /pdb:%SNAM%.pdb /OUT:%SNAM%.exe
 if exist %SNAM%.exe.manifest mt -nologo -manifest %SNAM%.exe.manifest -outputresource:%SNAM%.exe;1
